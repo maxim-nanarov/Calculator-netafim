0f5cd6b8f9dc9d314e951591f7872af916d4be63
student <you@example.com>
2023-01-16 08:59:29 +0200

    fixes to the databese

--- DB/sqlite.js
+++ DB/sqlite.js
@@ -26,17 +26,21 @@ db.serialize(async () => {
   // let sql = `CREATE TABLE IF NOT EXISTS Drippers(
   //   id INTERGER PRIMARY KEY,
   //   Dripper_Type STRING);`;
-  // let sql2 = `CREATE TABLE IF NOT EXISTS Pipes(
-  //     Models INTERGER PRIMARY KEY,
-  //     Diameter INTERGER,
-  //     Pressure INTERGER,
-  //     kd INTERGER);`;
-  // let sql3 = `CREATE TABLE IF NOT EXISTS Dripper_Pipes(
-  //       id INTERGER PRIMARY KEY,
-  //       Dripper_id INTERGER,
-  //       Pipe_Model INTERGER,
-  //       foreign key (Dripper_id) References Drippers(id) ON DELETE CASCADE ON UPDATE CASCADE,
-  //       foreign key (Pipe_Model) References Pipes(Models) ON DELETE CASCADE ON UPDATE CASCADE);`;
+  let sqlDelete1 = `Drop Table Dripper_Pipes;`;
+  let sqlDelete2 = `DROP TABLE Pipes;`;
+  db.run(sqlDelete1);
+  db.run(sqlDelete2);
+  let sql2 = `CREATE TABLE IF NOT EXISTS Pipes(
+      Models INTERGER PRIMARY KEY,
+      Diameter INTERGER,
+      Pressure INTERGER);`;
+  let sql3 = `CREATE TABLE IF NOT EXISTS Dripper_Pipes(
+        id INTERGER PRIMARY KEY,
+        Dripper_id INTERGER,
+        Pipe_Model INTERGER,
+        kd INTERGER,
+        foreign key (Dripper_id) References Drippers(id) ON DELETE CASCADE ON UPDATE CASCADE,
+        foreign key (Pipe_Model) References Pipes(Models) ON DELETE CASCADE ON UPDATE CASCADE);`;
   // let sql4 = `CREATE TABLE IF NOT EXISTS Drippers_data(
   //         Data_id INTERGER PRIMARY KEY,
   //         Dripper_id Interger,
@@ -45,14 +49,15 @@ db.serialize(async () => {
   //         Pressure INTERGER,
   //         foreign key (Dripper_id) References Drippers(id) ON DELETE CASCADE ON UPDATE CASCADE);`;
   // db.run(sql);
-  // db.run(sql2);
-  // db.run(sql3);
+  db.run(sql2);
+  db.run(sql3);
   // db.run(sql4);
   // let sql = `INSERT INTO Drippers VALUES('0','UniRam')`;
-  // let sql = `INSERT INTO PIPES VALUES ('16009','14.2','3.0','1.3'),('16010','14.2','3.5','1.3'),('16011','14.2','3.5','1.3'),('16012','14.2','4.0','1.3'),('20011','17.5','3.5','0.4'),('20012','17.5','4.0','0.4');`;
+  let sqlInsert1 = `INSERT INTO PIPES VALUES ('16009','14.2','3.0'),('16010','14.2','3.5'),('16011','14.2','3.5'),('16012','14.2','4.0'),('20011','17.5','3.5'),('20012','17.5','4.0');`;
   // let sql = `INSERT INTO DRIPPERS_DATA VALUES('0','0','0.7','0','0.7'),('1','0','1','0','1'),('2','0','1.6','0','1.6'),('3','0','2.3','0','2.3'),('4','0','3.5','0','3.5');`;
-  let sql = `INSERT INTO Dripper_Pipes Values ('0','0','16009'),('1','0','16010'),('2','0','16011'),('3','0','16012'),('4','0','20010'),('5','0','20012');`;
-  db.run(sql);
+  let sqlInsert2 = `INSERT INTO Dripper_Pipes Values ('0','0','16009','1.3'),('1','0','16010','1.3'),('2','0','16011','1.3'),('3','0','16012','1.3'),('4','0','20010','0.4'),('5','0','20012','0.4');`;
+  db.run(sqlInsert1);
+  db.run(sqlInsert2);
   // db.run(sql2);
   // Insert to table
   //   sql = `INSERT INTO Pipes `;
