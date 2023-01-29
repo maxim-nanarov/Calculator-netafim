37bf29172d73d2c5b785040ba9d26e9605b06415
student <you@example.com>
2023-01-29 09:09:18 +0200

    fixed the link from the server to the database, and the complete programm with the css

--- src/main/main.ts
+++ src/main/main.ts
@@ -142,16 +142,17 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 const App = express();
 //suppose to be the window directory to the data base:
-// let Path = path.join('win-unpacked', 'resources', 'test');
+let Path = path.join(__dirname, '../../../test.db');
 //
 //suppose to be the Linux directory to the data base:
 // let Path = path.join('linux-unpacked', 'resources', 'test.db');
 //
 //this is the localy directory of the database:
-let Path = path.join('assets', 'DB', 'test.db');
+// let Path = path.join('assets', 'DB', 'test.db');
 //assets/DB/test.db
 //In order to package for windows:  --win --x64
 //need to insert it to the json file in the package command
+//THE CHOSEN ONE: ../../../test.db
 const db = new sqlite3.Database(Path, (err: any) => {
   if (err) {
     console.error(err.message);
